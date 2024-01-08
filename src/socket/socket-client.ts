b5a6b1b191435c411e14f02a35b13d2aa78d6c71
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-08 02:41:53 +0000

    refactored logic inside socket-client and added a couple of Consumer Events

--- src/socket/socket-client.ts
+++ src/socket/socket-client.ts
@@ -1,6 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { Module, OnModuleInit } from "@nestjs/common";
+import { io, Socket } from 'socket.io-client';
 
 @Injectable({})
-export class SocketClient {
+export class SocketClient implements OnModuleInit {
+    public socketClient: Socket;
+
+    constructor() {
+        this.socketClient = io('http://localhost:3000')
+    }
+    onModuleInit() {
+        this.registerConsumerEvents()
+    }
+
+    private registerConsumerEvents() {
+        this.socketClient.on('connect', () => {
+            console.log('connected to server');
+        });
+
+        this.socketClient.on('onMessage', (payload: any) => {
+            console.log(payload);
+        });
+    }
 
 }
--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,18 +1,9 @@
-import { Module, OnModuleInit } from "@nestjs/common";
-import { io, Socket } from 'socket.io-client';
+import { Module } from "@nestjs/common";
+import { SocketClient } from "./socket-client";
 
 @Module({
-    providers: [SocketModule]
+    providers: [SocketClient]
 })
-export class SocketModule implements OnModuleInit {
-    public socketClient: Socket;
-
-    constructor() {
-        this.socketClient = io('http://localhost:3000')
-    }
-    onModuleInit() {
-        this.socketClient.on('connect', () => {
-            console.log('connected to server');
-        })
-    }
+export class SocketModule {
+   
 }
