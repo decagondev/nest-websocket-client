58d43546f22b5121d6794256e4aa08d042c337ce
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-08 02:17:37 +0000

    added a socket connection to the websocket server

--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,8 +1,18 @@
-import { Module } from "@nestjs/common";
+import { Module, OnModuleInit } from "@nestjs/common";
+import { io, Socket } from 'socket.io-client';
 
 @Module({
     providers: [SocketModule]
 })
-export class SocketModule {
+export class SocketModule implements OnModuleInit {
+    socketClient: Socket;
 
+    constructor() {
+        this.socketClient = io('http://localhost:3000')
+    }
+    onModuleInit() {
+        this.socketClient.on('connect', () => {
+            console.log('connected to server');
+        })
+    }
 }
